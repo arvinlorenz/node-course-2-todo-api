8b14b364b31f363d4acb06ab7dd4172fab78d1a7
arvin <arvinlorenz@gmail.com>
2018-03-13 10:07:13 +0800

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,24 @@
 var env = process.env.NODE_ENV || 'development';
-// console.log('env ****', env);
-if(env === 'development'){
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp2';
-} else if(env === 'test'){
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if(env === 'development' || env === 'test'){
+	var config = require('./config.json');
+	var envConfig = config[env]; //when using variable to access a property
+
+	Object.keys(envConfig).forEach((key)=>{
+		process.env[key] = envConfig[key];
+	});
+
+
 }
+
+
+
+
+
+// if(env === 'development'){
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp2';
+// } else if(env === 'test'){
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
